c3bb075a32501588da61069482fab511395aae94
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-04 18:21:27 +0400

    Update 231_power_of_two.py

--- 231_power_of_two.py
+++ 231_power_of_two.py
@@ -5,3 +5,10 @@ class Solution:
         while n % 2 == 0:
             n = n / 2
         return n == 1
+
+# Perfect one
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        return n & (n - 1) == 0
