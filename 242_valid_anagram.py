8e4593514a0f297749e6aa4ca58968ac585f65a3
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-04 18:24:21 +0400

    valid_anagram

---
+++ 242_valid_anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        lc = {}
+        tc = {}
+
+        for s1 in s:
+            if s1 in lc:
+                lc[s1] += 1
+            else:
+                lc[s1] = 1
+
+        for t1 in t:
+            if t1 in tc:
+                tc[t1] += 1
+            else:
+                tc[t1] = 1
+
+        return(lc == tc)
