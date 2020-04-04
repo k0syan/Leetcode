83e8f724158aaa558bd6610e1589459f2b2cf074
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-04 16:47:38 +0400

    Update 283_move_zeroes.py

--- 283_move_zeroes.py
+++ 283_move_zeroes.py
@@ -15,3 +15,17 @@ class Solution:
         for i in range(tn):
             nums.append(0)
         print(nums)
+
+# Second solution
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zi = -1
+        for i in range(len(nums)):
+            if nums[i] == 0 and zi == -1:
+                zi = i
+            if nums[i] != 0 and zi != -1:
+                nums[i], nums[zi] = nums[zi], nums[i]
+                zi += 1
