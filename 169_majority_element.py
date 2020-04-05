2cffcb843497c9a21a4cb1d578e0e6e52a84155e
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-05 17:01:51 +0400

    Update 169_majority_element.py

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -12,3 +12,15 @@ class Solution:
         return(key[0])
             
         
+# Boyer-Moore Voting Algorithm
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
