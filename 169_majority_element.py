8ae5780af1bd2b3ca6cdc9c05eefe120fd76988f
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-05 16:52:57 +0400

    169_majority_element

---
+++ 169_majority_element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        for n in nums:
+            if n in d:
+                d[n] += 1
+            else:
+                d[n] = 1
+        max_count = max(d.values())
+        print(max_count)
+        key = [k for k, v in d.items() if v == max_count]
+        return(key[0])
+            
+        
