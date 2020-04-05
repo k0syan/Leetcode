6ac073039045b5eeaaa61c56de886bb7acad03c7
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-05 22:58:17 +0400

    Update 191_number_of_1_bits.py

--- 191_number_of_1_bits.py
+++ 191_number_of_1_bits.py
@@ -6,4 +6,15 @@ class Solution:
             if a == "1":
                 c += 1
         return c
+    
+# With bit operations
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        c = 0
+    
+        while n:
+            n = n & (n - 1)
+            c += 1
+        return c
         
