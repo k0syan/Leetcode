15ca391a778e0afbb318859f6c94df8e6e7559c4
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-05 12:02:10 +0400

    Update 342_power_of_four.py

--- 342_power_of_four.py
+++ 342_power_of_four.py
@@ -1,12 +1,11 @@
 class Solution:
     def isPowerOfFour(self, num: int) -> bool:
-        n = num
-        if n == 1:
+        if num == 1:
             return True
-        if n <= 3:
+        if num <= 3:
             return False
-        f = sqrt(n) - round(sqrt(n)) == 0
-        return (n & (n - 1) == 0 and f)
+        f = sqrt(num) - round(sqrt(num)) == 0
+        return (num & (num - 1) == 0 and f)
 
 # Better solution
 
