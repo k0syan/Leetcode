71fdbd0938d9892720ccc38fb18747ccddadf329
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-06 16:20:47 +0400

    Update 389_find_the_difference.py

--- 389_find_the_difference.py
+++ 389_find_the_difference.py
@@ -6,3 +6,11 @@ class Solution:
             if s[i] != t[i]:
                 return t[i]
         return t[-1]
+    
+# XoR 
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        ans = 0
+        for c in s + t:
+            ans ^= ord(c)
+        return chr(ans)
