42a078fe405f5f0391ecb8b8e7bb363aebe818b8
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-07 14:30:31 +0400

    Update 448_find_all_numbers_disappeared_in_an_array.py

--- 448_find_all_numbers_disappeared_in_an_array.py
+++ 448_find_all_numbers_disappeared_in_an_array.py
@@ -2,3 +2,16 @@ class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         retList = list(range(1, len(nums) + 1))
         return list(set(retList) - set(nums))
+    
+# Without extra space
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            tmp = abs(nums[i]) - 1
+            if nums[tmp] > 0:
+                nums[tmp] *= -1
+        retList = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                retList.append(i + 1)
+        return retList
