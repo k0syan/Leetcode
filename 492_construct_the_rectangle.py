bec11ea509337be1624ae273569336ea39f0fc09
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-09 19:27:29 +0400

    Update 492_construct_the_rectangle.py

--- 492_construct_the_rectangle.py
+++ 492_construct_the_rectangle.py
@@ -1,10 +1,6 @@
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
-        l = w = int(sqrt(area))
-        while l * w != area:
-            if l * w < area:
-                l += 1
-            if l * w > area:
-                w -= 1
-        return (l, w) 
-            
+        w = int(sqrt(area))
+        while area % w != 0:
+            w -= 1
+        return (area // w, w)
