d3621e56463ebeb2e915464d58c0850c85ff2b70
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-09 20:47:28 +0400

    Update 575_distribute_candies.py

--- 575_distribute_candies.py
+++ 575_distribute_candies.py
@@ -9,4 +9,13 @@ class Solution:
                 cnt += 1
             i += 1
         return cnt
+    
+# set solution
+
+class Solution:
+    def distributeCandies(self, candies: List[int]) -> int:
+        d = set()
+        for c in candies:
+            d.add(c)
+        return min(len(candies) // 2, len(d))
             
