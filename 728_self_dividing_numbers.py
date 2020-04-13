898813c10a0707186d67f4b1d2d4d0f23c353a2b
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-14 01:11:39 +0400

    Update 728_self_dividing_numbers.py

--- 728_self_dividing_numbers.py
+++ 728_self_dividing_numbers.py
@@ -19,3 +19,8 @@ class Solution:
             if Solution.check(i):
                 ans.append(i)
         return ans
+    
+# One liner
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        return [x for x in range(left, right+1) if all((i and (x % i==0) for i in map(int, str(x))))]
