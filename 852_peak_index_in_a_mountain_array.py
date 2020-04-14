b8b8ab14357e6bf917492aa65bc8c2f57ec0fec0
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-04-14 14:50:54 +0400

    Update 852_peak_index_in_a_mountain_array.py

--- 852_peak_index_in_a_mountain_array.py
+++ 852_peak_index_in_a_mountain_array.py
@@ -3,3 +3,15 @@ class Solution:
         for i in range(len(A) - 1):
             if A[i] > A[i + 1]:
                 return i
+            
+# Binary search
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        s, e = 0, len(A) - 1
+        while s < e:
+            mid = (s + e) // 2
+            if A[mid] < A[mid + 1]:
+                s = mid + 1
+            else:
+                e = mid
+        return s
