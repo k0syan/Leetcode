4dac54c2a87730ea74abc30a709c8c0ccef93a15
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-05-04 12:40:00 +0400

    Delete 1009_complement_of_10_integer.py

--- 1009_complement_of_10_integer.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def bitwiseComplement(self, N: int) -> int:
-        b = "{0:b}".format(N)
-        nb = ""
-        for c in b:
-            nb += "1"
-        return int(nb, 2) ^ N
