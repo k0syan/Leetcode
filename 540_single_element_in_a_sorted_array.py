6dd9d5f7d03f9ea58097bc7676634ac6f190599d
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-05-12 12:52:35 +0400

    Update 540_single_element_in_a_sorted_array.py

--- 540_single_element_in_a_sorted_array.py
+++ 540_single_element_in_a_sorted_array.py
@@ -1,3 +1,4 @@
+# Greedy
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         c = nums[0]
@@ -18,3 +19,17 @@ class Solution:
         for n in nums:
             ans ^= n
         return ans
+
+# Binary search
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        s, e = 0, len(nums) - 1
+        while s < e:
+            m = (s + e) // 2
+            if m % 2:
+                m -= 1
+            if nums[m] != nums[m + 1]:
+                e = m
+            else:
+                s = m + 2
+        return nums[s]
