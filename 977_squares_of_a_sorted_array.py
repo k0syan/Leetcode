6b4f903340c6349a034912a9f49c96862a94c4e8
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-05-12 13:14:58 +0400

    Update 977_squares_of_a_sorted_array.py

--- 977_squares_of_a_sorted_array.py
+++ 977_squares_of_a_sorted_array.py
@@ -2,3 +2,18 @@
 class Solution:
     def sortedSquares(self, A: List[int]) -> List[int]:
         return sorted(x * x for x in A)
+    
+# Better
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        res = [0] * len(A)
+        s, e = 0, len(A) - 1
+        while s <= e:
+            start, end = abs(A[s]), abs(A[e])
+            if start > end:
+                res[e - s] = start * start
+                s += 1
+            else:
+                res[e - s] = end * end
+                e -= 1
+        return res
