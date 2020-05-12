96762e900bbe7856c0f7d4a18928777c0a12dc58
Shahen Kosyan <k0syan@users.noreply.github.com>
2020-05-12 17:18:10 +0400

    Update 1108_defanging_an_ip_address.py

--- 1108_defanging_an_ip_address.py
+++ 1108_defanging_an_ip_address.py
@@ -2,3 +2,8 @@ class Solution:
     def defangIPaddr(self, address: str) -> str:
         address = address.replace('.', '[.]')
         return address
+    
+# One liner
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return '[.]'.join(address.split('.'))
